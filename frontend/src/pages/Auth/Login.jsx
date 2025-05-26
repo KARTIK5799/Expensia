c0ec14519667d09ff0901d66077dabbc57f3d91c
KARTIK5799 <thekartiksapkal@gmail.com>
2025-05-26 05:58:28 +0530

    Inialize the state for login

--- frontend/src/pages/Auth/Login.jsx
+++ frontend/src/pages/Auth/Login.jsx
@@ -1,7 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import AuthLayout from '../../componentes/layouts/AuthLayout'
 
 const Login = () => {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [error, setError] = useState();
   return (
     <AuthLayout>
     <div>
